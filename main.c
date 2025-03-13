dd3153036ac36471307ce43f1b1eccd6c2f2c951
kenny <kenny@linux.com>
2025-03-13 18:16:04 +0100

    added first arg input + improved install script

--- main.c
+++ main.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
+int main(int argc, char *argv[]){
 
+
+  //check if user input is not empty
+  if(argc > 1){
+    char *webSite = argv[1];
+    printf("%s\n", webSite);
+  }
+
+  //start firefox *github.com just a place holder BETA*
   int firefoxStartStatus = system("firefox github.com &");
 
   if(firefoxStartStatus >= 0){
