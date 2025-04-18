c1359d77c255675c702ae286ff47fb9d55fa2ccc
kenny <kenny@linux.com>
2025-04-18 09:45:18 +0200

    Clean up

--- scripts/install.py
+++ scripts/install.py
@@ -4,8 +4,8 @@ print("Installation working...")
 
 #//enter here the location where you git cloned the project
 #//example:
-os.chdir("/home/user/Document/goweb")
-createSTATUS = os.system("gcc -o gowebBETA main.c cJson/cJSON.c")
+os.chdir("/home/user/Docs/goweb")
+createSTATUS = os.system("gcc -o goweb main.c cJson/cJSON.c")
 mvSTATUS = os.system("sudo mv gowebBETA /usr/local/bin/")
 
 if (createSTATUS == 0 and mvSTATUS == 0):
