7aabb52d33de403f7de45fa8a771df9a3dd87434
KennyoDev <twier@tutamail.com>
2025-07-27 16:24:22 +0200

    Update main.c

--- main.c
+++ main.c
@@ -21,15 +21,15 @@ bool getBrowserSpecified(const char flag[3]){
 
 int main(int argc, char *argv[]){
 
-//check if not empty
-if (argc < 2) {
-    printf("Some worng, type the site!");
-    return 1;
-}
+  //check if not empty
+  if (argc < 2) {
+      printf("Some wrong, type the site!");
+      return 1;
+  }
 
   char command[256];
-  //go thru every char and make lowercase
-
+  
+  //go through every char and make it lowercase
   for(int i = 1; i < argc; i++){
     for (int j = 0; argv[i][j] != '\0'; j++) {
         argv[i][j] = tolower(argv[i][j]);
@@ -40,8 +40,7 @@ if (argc < 2) {
 
     addUrlToJSON(argv[2], argv[3]);
 
-  }
-  else{
+  }else{
 
     char *siteURL;
 
